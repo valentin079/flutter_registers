03b59f668a38d16ff22f52e2fee149f82cc134f6
valentin079 <emerson.oliveira02@academico.ifs.edu.br>
2023-04-23 10:18:06 -0300

    FLutter application

---
+++ lib/services/journal_service.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
+
+import '../models/journal.dart';
+
+class JournalService {
+  // Consiga seu IP usando o comando "ipconfig" no Windows ou "ifconfig" no Linux.
+  static const String url = "http://192.168.0.107:3000/";
+  static const String resource = "journals/";
+
+  http.Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
+
+  String getURL() {
+    return "$url$resource";
+  }
+
+  Uri getUri() {
+    return Uri.parse(getURL());
+  }
+
+  Future<bool> register(Journal journal) async {
+    String journalJSON = json.encode(journal.toMap());
+
+    http.Response response = await client.post(
+      getUri(),
+      headers: {'Content-type': 'application/json'},
+      body: journalJSON,
+    );
+
+    if (response.statusCode == 201) {
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<List<Journal>> getAll() async {
+    http.Response response = await client.get(getUri());
+
+    if (response.statusCode != 200) {
+      //TODO: Criar uma exceção personalizada
+      throw Exception();
+    }
+
+    List<Journal> result = [];
+
+    List<dynamic> jsonList = json.decode(response.body);
+    for (var jsonMap in jsonList) {
+      result.add(Journal.fromMap(jsonMap));
+    }
+
+    return result;
+  }
+}
